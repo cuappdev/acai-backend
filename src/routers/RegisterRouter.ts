ffbfb652d29f94836996da3344480da7077c2570
Keivan Shahida <keivanshahida@icloud.com>
2019-03-20 17:41:18 -0400

    Validate email and phone number

--- src/routers/RegisterRouter.ts
+++ src/routers/RegisterRouter.ts
@@ -1,5 +1,7 @@
 import { randomBytes } from 'crypto';
+import { validate } from 'email-validator';
 import { Request, Response } from 'express';
+import { parsePhoneNumberFromString } from 'libphonenumber-js';
 
 import ApplicationRouter from '../appdev/ApplicationRouter';
 import UsersRepo from '../repos/UsersRepo';
@@ -15,17 +17,24 @@ class RegisterRouter extends ApplicationRouter<Object> {
 
   async content(req: Request): Promise<Object> {
     const { email, password, firstName, lastName, phoneNumber } = req.body;
-
-    const user = await UsersRepo.createUser({
-      email,
-      password,
-      firstName,
-      lastName,
-      phoneNumber,
-      // TODO: Get customerId from Square API
-      customerId: randomBytes(32).toString('hex'),
-    });
-    return UsersRepo.getUserById(user.id);
+    if (email && password && firstName && lastName && phoneNumber) {
+      const parsedPhoneNumber = parsePhoneNumberFromString(phoneNumber, 'US');
+      if (parsedPhoneNumber.isValid() && validate(email)) {
+        const formattedPhoneNumber = parsedPhoneNumber.formatNational();
+        const user = await UsersRepo.createUser({
+          email,
+          password,
+          firstName,
+          lastName,
+          phoneNumber: formattedPhoneNumber,
+          // TODO: Get customerId from Square API
+          customerId: randomBytes(32).toString('hex'),
+        });
+        return UsersRepo.getUserById(user.id);
+      }
+      throw Error('Invalid phone number or email');
+    }
+    throw Error('Missing required field');
   }
 }
 
